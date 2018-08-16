8c305fad8c82bf52142e46f37356e3cd45830edd
Student <example@example.com>
2018-08-16 11:31:44 -0700

    add pipe filter and done/completed tasks

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,6 +6,7 @@ import { AppComponent } from './app.component';
 import { TaskListComponent } from './task-list/task-list.component';
 import { EditTaskComponent } from './edit-task/edit-task.component';
 import { NewTaskComponent } from './new-task/new-task.component';
+import { CompletenessPipe } from './completeness.pipe';
 
 
 @NgModule({
@@ -13,7 +14,8 @@ import { NewTaskComponent } from './new-task/new-task.component';
     AppComponent,
     TaskListComponent,
     EditTaskComponent,
-    NewTaskComponent
+    NewTaskComponent,
+    CompletenessPipe
   ],
   imports: [
     BrowserModule,
